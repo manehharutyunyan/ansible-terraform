16a6d53fa1ba18680bd10c71cb2ba923a609d1d3
manehharutyunyan <maneh.harutyunyan@gmail.com>
2023-05-13 23:57:38 +0400

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,5 +11,5 @@ variable "aws_secret_access_key" {
 provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
-  region     = "us-west-2"
+  region     = "us-east-2"
 }
